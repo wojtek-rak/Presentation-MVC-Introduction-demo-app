a8d9f65fc29d362f2561ba44237396a36e3f2883
Wolix <Rak.wojtek@wp.pl>
2018-10-08 23:25:24 +0200

    Initial commit

---
+++ FirstTestApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
